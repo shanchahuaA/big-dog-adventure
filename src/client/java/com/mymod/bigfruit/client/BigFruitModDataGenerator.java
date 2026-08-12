66418d8d3618fa6f3e65a6d2994eca0eb23aafaf
shanchahuaA <1755952092@qq.com>
2026-08-13 04:03:42 +0800

    这是一个测试2

---
+++ src/client/java/com/mymod/bigfruit/client/BigFruitModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.mymod.bigfruit.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BigFruitModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
